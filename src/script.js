1df5b2e746a6f7387fd1fd6b9ece708b9b4fb0e7
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-05-11 16:13:41 -0400

    Js updated for innerHTML select

--- src/script.js
+++ src/script.js
@@ -26,3 +26,26 @@ bogotaTimeElement.innerHTML = bogotaTime.format("h:mm:ss[<span>]A[</span>]");
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+
+
+function updateCity(event){
+    let cityTz = event.target.value;
+    let cityName = cityTz.replace("_"," ").split("/")[1];
+    let cityTime = moment().tz(cityTz);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = 
+    `
+    <div class="city" id="berlin">
+        <div class="city-name">${cityName}</div>
+        <div class="date-city">${cityTime.format("MMMM Do YYYY")}</div>
+        <div class="time-city"${cityTime.format("h:mm:ss[<span>]A[</span>]")}></div>
+    </div>
+    `;
+}
+
+
+let citySelectElement = document.querySelector("#select-city");
+
+citySelectElement.addEventListener("change", updateCity);
+
